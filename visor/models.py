e3ca47f3c82e206b75786a1abf127d7f8b261972
joseampuero <ampuero.jose96@gmail.com>
2022-02-22 14:45:12 -0300

    serializers

--- visor/models.py
+++ visor/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Text:
+     def __init__(self, pages, fromPage, toPage):
+        self.content = pages
+        self.fromPage = fromPage
+        self.toPage = toPage
